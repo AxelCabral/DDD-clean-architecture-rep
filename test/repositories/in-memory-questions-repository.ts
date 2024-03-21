d31f2e1b55fe098ed076fb35c8c809c9efae0882
Axel Cabral <axel.cabral0102@gmail.com>
2024-03-21 00:28:08 -0300

    Updt 2.7
    - Criação da notificação para caso de uso de nova melhor resposta escolhida

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
@@ -32,6 +33,8 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async create(question: Question) {
     this.items.push(question)
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async delete(question: Question) {
@@ -42,6 +45,8 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     this.questionAttachmentsRepository.deleteManyByQuestionId(
       question.id.toString(),
     )
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async save(question: Question) {
